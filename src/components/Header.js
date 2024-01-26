50327c7d0c886691baa34f01279d05100f4633a0
Ramanuj <ramanuj0705@gmail.com>
2024-01-26 12:44:32 +0530

    Designed sidebar of me-tube

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,40 +2,40 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="grid grid-flow-col p-4 m-2 shadow-lg">
-      <div className="flex col-span-2">
+    <div className="grid grid-flow-col py-2 px-4 shadow-lg">
+      <div className="flex col-span-2 items-center">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png"
           alt="hamburger icon"
-          className="h-8"
+          className="h-6 items-center"
         />
         <img
           src="https://t3.ftcdn.net/jpg/03/00/38/90/360_F_300389025_b5hgHpjDprTySl8loTqJRMipySb1rO0I.jpg"
           alt="logo"
-          className="h-10 ml-4"
+          className="h-14 ml-4 items-center"
         />
       </div>
 
-      <div className="flex col-span-9 ">
+      <div className="flex col-span-9 items-center">
         <input
           type="text"
           placeholder="Search"
-          className="h-8 w-1/2 border border-black rounded-lg p-2"
+          className="h-8 w-1/2 border border-black rounded-l-full p-2"
         />
-        <button>
+        <button className="">
           <img
             src="https://cdn3.iconfinder.com/data/icons/feather-5/24/search-512.png"
             alt="search icon"
-            className="h-6 pl-2"
+            className="h-8 px-5 py-1 items-center border border-black rounded-r-full bg-gray-100"
           />
         </button>
       </div>
 
-      <div className="col-span-1">
+      <div className="flex col-span-1 items-center justify-end mr-8">
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9hrpUnDCcHPqBT1mvnZ0HM-CddsZnfZXiyJd_VPRGOA&s"
           alt="user icon"
-          className="h-8"
+          className="h-8 items-center"
         />
       </div>
     </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,42 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="p-4 shadow-lg col-span-2">
+      <div className="mb-6">
+        <ul className="flex flex-col gap-2 border-b-2 py-4">
+          <li>Home</li>
+          <li>Shorts</li>
+          <li>Subscriptions</li>
+        </ul>
+      </div>
+
+      <div className="mb-6">
+        <h1 className="font-bold pb-4">Explore</h1>
+        <ul className="flex flex-col gap-2 border-b-2 py-4">
+          <li>Trending</li>
+          <li>Shopping</li>
+          <li>Music</li>
+          <li>Live</li>
+          <li>Movies</li>
+          <li>Gaming</li>
+          <li>News</li>
+          <li>Sports</li>
+        </ul>
+      </div>
+
+      <div>
+        <h1 className="font-bold pb-4">Watch Later</h1>
+        <ul className="flex flex-col gap-2 border-b-2 py-4">
+          <li>Music</li>
+          <li>Movies</li>
+          <li>Gaming</li>
+          <li>News</li>
+          <li>Sports</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
