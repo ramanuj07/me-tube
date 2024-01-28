4bd14aad6b4a7a18f242eec5cb3d2e423478dcfd
Ramanuj <ramanuj0705@gmail.com>
2024-01-28 17:46:35 +0530

    Live chat built using Long-polling

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/navSlice";
 import { useSearchParams } from "react-router-dom";
 import CommentsContainer from "./CommentsContainer";
+import LiveChat from "./LiveChat";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
@@ -13,17 +14,23 @@ const WatchPage = () => {
   }, []);
 
   return (
-    <div>
-      <div className="p-5">
-        <iframe
-          width="900"
-          height="500"
-          src={`https://www.youtube.com/embed/${searchParams.get("v")}`}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
+    <div className="flex flex-col">
+      <div className="flex p-5">
+        <div>
+          <iframe
+            width="900"
+            height="500"
+            src={`https://www.youtube.com/embed/${searchParams.get("v")}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+
+        <div>
+          <LiveChat />
+        </div>
       </div>
 
       <CommentsContainer />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 const GOOGLE_API_KEY = "AIzaSyBRFaHAnr8yAYD9uDVZNSc2mFZcN3VmKRs";
 
+export const OFFSET_LIVE_CHAT = 20;
+
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${GOOGLE_API_KEY}`;
 
 export const YOUTUBE_SEARCH_API =
